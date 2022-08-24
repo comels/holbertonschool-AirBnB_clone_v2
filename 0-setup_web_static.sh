629f4c3a6618827fb5a5d8713bbfded19bb5c2d6
comels <come.l.s@hotmail.fr>
2022-08-24 10:00:48 +0000

    setup

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# Script that sets up your web servers for the deployment of web_static
+
+if [ ! -x /usr/sbin/nginx ]; then
+    apt-get update
+    apt-get -y upgrade
+    apt-get -y install nginx
+fi
+
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+
+# mkdir -p /data/web_static/{releases/test, shared}
+
+echo "Hello Word !" > /data/web_static/releases/test/index.html
+
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+chown -R ubuntu:ubuntu /data
+
+sed -i '/:80 default_server;/a \\tlocation /hbnb_static/ { \n\t\t alias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+
+service nginx restart
