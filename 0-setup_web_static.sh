0a50f75a28d609e066687c452148d77a5e68b802
comels <come.l.s@hotmail.fr>
2022-08-25 10:11:04 +0000

    sed

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -18,6 +18,6 @@ ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 chown -R ubuntu:ubuntu /data
 
-sed -i '/:80 default_server;/a \\tlocation /hbnb_static/ { \n\t\t alias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+sed -i '/listen 80 default_server;/a \\tlocation /hbnb_static/ { \n\t\t alias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 
 service nginx restart
