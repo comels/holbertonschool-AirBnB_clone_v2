c72f5090965616746cc10395507cde541421cb6a
comels <come.l.s@hotmail.fr>
2022-09-09 12:13:40 +0000

    format

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -59,7 +59,7 @@ class FileStorage:
     def delete(self, obj=None):
         """function that deletes an object"""
         if obj:
-            del(FileStorage.__objects[f"{obj.__class__.__name__}.{obj.id}"])
+            del(FileStorage.__objects["{}.{}".format(obj.__class__.__name__, obj.id)])
 
     def close(self):
         """ Function that call the reload method """
