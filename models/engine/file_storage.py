ba42e939d4dd8068c10c8cc9da3169f43a920e84
comels <come.l.s@hotmail.fr>
2022-09-10 08:39:44 +0000

    pycode

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -59,7 +59,8 @@ class FileStorage:
     def delete(self, obj=None):
         """function that deletes an object"""
         if obj:
-            del(FileStorage.__objects["{}.{}".format(obj.__class__.__name__, obj.id)])
+            del(FileStorage.__objects["{}.{}".format
+                (obj.__class__.__name__, obj.id)])
 
     def close(self):
         """ Function that call the reload method """
